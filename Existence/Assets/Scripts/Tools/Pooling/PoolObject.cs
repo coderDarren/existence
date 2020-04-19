2e276440588d42898eaad1317b71ad1a97c4f28d
Darren O'Neale <doneale3@gmail.com>
2020-04-19 13:37:48 -0400

    player controller ground checks fixed. pooling system added

---
+++ Existence/Assets/Scripts/Tools/Pooling/PoolObject.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+namespace Tools.Game.Pooling {
+    public class PoolObject : MonoBehaviour
+    {
+
+        protected Pool m_Pool;
+
+#region Public Functions
+        public void Enable() {
+            OnObjectEnable();
+        }
+
+        public void Disable() {
+            OnObjectDisable();
+        }
+#endregion
+
+#region Overridable Functions
+        protected virtual void OnObjectEnable() {
+            m_Pool = transform.parent.GetComponent<Pool>();
+
+            if (!m_Pool) {
+                Debug.LogWarning("Pool object does not have a valid reference to its pool.");
+            }
+        }
+
+        protected virtual void OnObjectDisable() {
+
+        }
+#endregion
+    }
+}
