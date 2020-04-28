9ab7e7784f936e213f19fb0fae48e20b1153dff8
Darren O'Neale <doneale3@gmail.com>
2020-04-27 22:12:31 -0400

    cursors added for window manip

--- Existence/Assets/Scripts/Game/Cursor/CursorController.cs
+++ Existence/Assets/Scripts/Game/Cursor/CursorController.cs
@@ -1,18 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CursorController : MonoBehaviour
+public class CursorController : GameSystem
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static CursorController instance;
+
+    public Image cursor;
+    public Sprite main;
+    public Sprite scaleHorizontal;
+    public Sprite scaleVertical;
+    public Sprite scaleDiagRight;
+    public Sprite scaleDiagLeft;
+    public Sprite drag;
+
+    private RectTransform m_Rect;
+
+#region Unity Functions
+    private void Awake() {
+        if (!instance) {
+            instance = this;
+            Cursor.visible = false;
+            m_Rect = cursor.GetComponent<RectTransform>();
+            LoadMain();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        Vector3 _cursorPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        _cursorPos.x *= Screen.width;
+        _cursorPos.y *= Screen.height;
+        transform.position = _cursorPos;
     }
+#endregion
+
+#region Public Functions
+    public void LoadMain() {
+        cursor.sprite = main;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.up;
+        m_Rect.localScale = Vector2.one;
+    }
+
+    public void LoadScaleHorizontal() {
+        cursor.sprite = scaleHorizontal;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.one * 0.5f;
+        m_Rect.localScale = Vector2.one * 1.35f;
+    }
+
+    public void LoadScaleVertical() {
+        cursor.sprite = scaleVertical;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.one * 0.5f;
+        m_Rect.localScale = Vector2.one * 1.35f;
+    }
+
+    public void LoadScaleDiagRight() {
+        cursor.sprite = scaleDiagRight;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.one * 0.5f;
+        m_Rect.localScale = Vector2.one * 1.35f;
+    }
+
+    public void LoadScaleDiagLeft() {
+        cursor.sprite = scaleDiagLeft;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.one * 0.5f;
+        m_Rect.localScale = Vector2.one * 1.35f;
+    }
+
+    public void LoadDrag() {
+        cursor.sprite = drag;
+        cursor.material.mainTexture = cursor.sprite.texture;
+        m_Rect.pivot = Vector2.one * 0.5f;
+        m_Rect.localScale = Vector2.one * 1.35f;
+    }
+#endregion
 }
--- Existence/Assets/Scripts/Tools/Menu/UIHandle.cs
+++ Existence/Assets/Scripts/Tools/Menu/UIHandle.cs
@@ -8,8 +8,11 @@ using UnityEngine.EventSystems;
 ///     1. Resizing
 ///     2. Dragging
 /// </summary>
-public class UIHandle : MonoBehaviour, 
+public class UIHandle : GameSystem, 
+                        IPointerEnterHandler,
+                        IPointerExitHandler,
                         IPointerDownHandler, 
+                        IPointerUpHandler,
                         IDragHandler
 {
     public enum HandleType {
@@ -34,6 +37,21 @@ public class UIHandle : MonoBehaviour,
     private UIContainer m_Target;
     private HandleLoc m_Loc;
     private Vector2 m_Offset;
+    private CursorController m_Cursor;
+    private bool m_PointerDown;
+    private bool m_Hovering;
+
+    private CursorController cursor {
+        get {
+            if (!m_Cursor) {
+                m_Cursor = CursorController.instance;
+            }
+            if (!m_Cursor) {
+                LogError("Trying to use cursor, but no instance was found.");
+            }
+            return m_Cursor;
+        }
+    }
 
 #region Unity Functions
 #endregion
@@ -46,7 +64,43 @@ public class UIHandle : MonoBehaviour,
 #endregion
 
 #region Interface Functions
+    public void OnPointerEnter(PointerEventData _ped) {
+        m_Hovering = true;
+
+        if (!cursor) return;
+        
+        switch (m_Loc) {
+            case HandleLoc.IRRELEVANT: cursor.LoadDrag(); break;
+            case HandleLoc.BOTTOM_RIGHT:
+            case HandleLoc.TOP_LEFT: cursor.LoadScaleDiagRight(); break;
+            case HandleLoc.BOTTOM_LEFT:
+            case HandleLoc.TOP_RIGHT: cursor.LoadScaleDiagLeft(); break;
+            case HandleLoc.BOTTOM:
+            case HandleLoc.TOP: cursor.LoadScaleVertical(); break;
+            case HandleLoc.RIGHT:
+            case HandleLoc.LEFT: cursor.LoadScaleHorizontal(); break;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData _ped) {
+        m_Hovering = false;
+
+        if (!cursor) return;
+        if (m_PointerDown) return;
+        cursor.LoadMain();
+    }
+
+    public void OnPointerUp(PointerEventData _ped) {
+        m_PointerDown = true;
+
+        if (!cursor) return;
+        if (m_Hovering) return;
+        cursor.LoadMain();
+    }
+
     public void OnPointerDown(PointerEventData _ped) {
+        m_PointerDown = false;
+
         if (!m_Target) return;
         m_Offset = new Vector2(m_Target.rect.transform.position.x, m_Target.rect.transform.position.y) - _ped.position;
     }
