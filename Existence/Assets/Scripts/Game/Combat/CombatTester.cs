1982fbd14ae785d86e5a8811589fdb3ac31166e8
Boxcar87 <Boxcar87@gmail.com>
2020-05-05 18:42:09 -0400

    Added attack loop logic with basic animation/effect

--- Existence/Assets/Scripts/Game/Combat/CombatTester.cs
+++ Existence/Assets/Scripts/Game/Combat/CombatTester.cs
@@ -1,17 +1,58 @@
 ﻿
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class CombatTester : GameSystem
 {
     public KeyCode damageTestKey;
+    public GameObject player;
+    
+    private Animator anim;
+    private ParticleSystem[] particles;
+    private bool attacking;
+    private float timer;
     
 
 #region Unity Functions
-    private void Update() {
+
+    private void Start(){
+        attacking = false;
+    }
+
+    private async void Update() {
+        anim = player.GetComponent<Animator>();
+        particles = player.GetComponentsInChildren<ParticleSystem>();
+        timer += Time.deltaTime;
+        
         if (!GameObject.FindGameObjectWithTag("CombatTestDummy")) return;
         if (Input.GetKeyDown(damageTestKey)) {
-            GameObject.FindGameObjectWithTag("CombatTestDummy").GetComponent<Mob>().Hit(50);
+            if(!attacking){
+                attacking = true;
+                anim.SetBool("attacking", true);
+                foreach(ParticleSystem particle in particles){
+                    particle.Play();                    
+                }                
+                timer = 1;
+            }
+            else{
+                attacking = false;
+                anim.SetBool("attacking", false);
+                foreach(ParticleSystem particle in particles){
+                    particle.Stop();
+                }
+                
+            }
         }
+        if(attacking){    
+           
+                
+                if(timer >= 2){
+                    GameObject.FindGameObjectWithTag("CombatTestDummy").GetComponent<Mob>().Hit(50);
+                    
+                    timer = 0;
+                }
+        Debug.Log(particles.Length);    
+       }
     }
 #endregion
 }
