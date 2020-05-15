2dbddaf1a01f511d0107d5a55c6c68a88f10a0f3
Darren O'Neale <doneale3@gmail.com>
2020-05-15 19:04:55 -0400

    added createAccount api. added load test on auth endpoint

---
+++ Existence/Assets/Scripts/Views/Login/AccountCreationPage.cs
@@ -0,0 +1,64 @@
+﻿using UniRx.Async;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityCore.Menu;
+
+public class AccountCreationPage : Page
+{
+    public Text statusLabel;
+    public InputField firstName;
+    public InputField lastName;
+    public InputField username;
+    public InputField password;
+    public InputField passwordConfirm;
+
+    private LoginController m_Controller;
+
+    private LoginController controller {
+        get {
+            if (!m_Controller) {
+                m_Controller = LoginController.instance;
+            }
+            if (!m_Controller) {
+                Log("Trying to access LoginController, but no instance was found.");
+            }
+            return m_Controller;
+        }
+    }
+
+#region Public Functions
+    public void Create() {
+        if (!controller) return;
+        //controller.Play();
+    }
+
+    public void GoBack() {
+        if (!controller) return;
+        //controller.GoToLogin();
+    }
+#endregion
+
+#region Private Functions
+   
+
+    private void ShowMessage(string _msg) {
+        controller.ShowMessage(statusLabel, _msg);
+    }
+
+    private void StopShowingMessage() {
+        //controller.StopShowingMessage();
+    }
+#endregion
+
+#region Override Functions
+    protected override void OnPageEnabled() {
+        base.OnPageEnabled();
+    }
+
+    protected override void OnPageDisabled() {
+        base.OnPageDisabled();
+        StopShowingMessage();
+    }
+#endregion
+}
--- db/queries/insertAccount.sql
+++ db/queries/insertAccount.sql
@@ -1,4 +1,4 @@
 #insert into accounts (username, first_name, last_name, apiKey)
 #values ('kyle123', 'kyle', 'oneale', 'a1b2c3key');
-
+#alter table accounts add column email varchar(255) unique;
 SELECT * FROM existence.accounts;
