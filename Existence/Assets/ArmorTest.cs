6983a46e775136d3c9738f40f13ccfb64da609f3
Boxcar87 <Boxcar87@gmail.com>
2020-05-23 22:22:56 -0400

    Created forearm prosthetic

--- Existence/Assets/ArmorTest.cs
+++ Existence/Assets/ArmorTest.cs
@@ -5,15 +5,18 @@ using UnityEngine;
  
  public  class ArmorTest : MonoBehaviour
  {
-    public GameObject augment;
-    
+    public GameObject prosthetic;
+    public GameObject player;
+
+    private Transform bone;
     private Vector3[] vert;
     private Vector3[] baseVert;
     private Mesh mesh;
 
     void Start(){
-        mesh = GetComponent<MeshFilter>().mesh;
+        mesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
         baseVert = mesh.vertices;
+        bone = player.transform.Find("mixamorig:LeftForeArm");
     }
     
     void Update(){
@@ -28,14 +31,15 @@ using UnityEngine;
     void EquipArmor(){
         
         vert = mesh.vertices;
-        Debug.Log("ping");
-
         for(int i = 0; i < vert.Length; i++){
-            vert[i] -= mesh.normals[i] * 2f;
+            vert[i] -= mesh.normals[i] * 0.1f * 0.1f * 0.1f * 0.2f;
             Debug.Log(vert[i]);
         }
-        
         mesh.vertices = vert;
+        
+        Instantiate(prosthetic, bone.position, bone.rotation);
+        
+
     }
 
     void ResetMesh(){
