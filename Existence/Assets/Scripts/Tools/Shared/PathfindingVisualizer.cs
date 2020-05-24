fa5c14f15777b0963d8d1e6e74a6a3c6af03db9b
Darren O'Neale <doneale3@gmail.com>
2020-05-23 21:57:16 -0400

    graph exporting finished

--- Existence/Assets/Scripts/Tools/Shared/PathfindingVisualizer.cs
+++ Existence/Assets/Scripts/Tools/Shared/PathfindingVisualizer.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+using Tools.IO;
 #endif
+
 /// <summary>
 /// PathfindingVisualizer is used to create a visual representation of node-edge based graph
 /// Right now, we use this to help us visualize graphs for server pathfinding
@@ -10,90 +11,85 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class PathfindingVisualizer : GameSystem
 {
-    public GameObject GraphNodeHandle;
-    public Graph graph;
-
-    private Hashtable m_NodeHash;
-    private Hashtable m_VisitedNodes;
     
 #region Unity Functions
-    /*private void Update() {
+    private void Update() {
         if (!debug) return;
-        if (Application.isPlaying) return;
-        m_NodeHash = new Hashtable();
-        Draw(transform);
-    }*/
-#endregion
-
-#region Public Functions
-    public void Add(Node _node) {
-        if (graph == null) {
-            graph = new Graph();
-        }
-        if (m_NodeHash == null) {
-            m_NodeHash = new Hashtable();
-        }
-        
-        if (m_NodeHash.ContainsKey(_node.objectId)) {
-            Debug.Log("node already registered: "+_node.objectId);
-            return;
-        }
-
-        m_NodeHash.Add(_node.objectId, _node);
-        graph.AddNode(_node);
-    }
-
-    public void Link(int _obj1, int _obj2) {
-        if (!m_NodeHash.ContainsKey(_obj1)) {
-            Log("No node found with id: "+_obj1);
-            return; 
-        }
-        if (!m_NodeHash.ContainsKey(_obj2)) {
-            Log("No node found with id: "+_obj2);
-            return;
-        }
-
-        Node _n1 = (Node)m_NodeHash[_obj1];
-        Node _n2 = (Node)m_NodeHash[_obj2];
-        graph.LinkNodes(_n1, _n2);
-
-        m_VisitedNodes = new Hashtable();
-        UpdateVisuals(graph.nodes[0]);
-    }
-
-    public void UpdateVisuals(Node _n) {
-        if (!m_VisitedNodes.ContainsKey(_n.objectId)) {
-            m_VisitedNodes.Add(_n.objectId, _n);
-        } else {
-            return;
-        }
-
-        foreach (Edge _e in _n.edges) {
-            Debug.Log("drawing edge");
-            Debug.DrawLine(new Vector3(_e.from.pos.x,_e.from.pos.y,_e.from.pos.z),new Vector3(_e.to.pos.x,_e.to.pos.y,_e.to.pos.z), Color.green);
-            UpdateVisuals(_e.to);
-        }
+        //if (Application.isPlaying) return;
+        Draw();
     }
 #endregion
 
 #region Private Functions
-    
+    private void Draw() {
+        foreach (Transform _t in transform) {
+            if (_t == transform) continue;
+            GraphNode _node = _t.GetComponent<GraphNode>();
+            if (_node == null) return;
+            DrawNode(_node);
+        }
+    }
+
+    private void DrawNode(GraphNode _node) {
+        foreach (GraphNode _neighbor in _node.neighbors) {
+            if (!_neighbor) continue;
+            DrawEdge(_node.transform.position, _neighbor.transform.position);
+            DrawNode(_neighbor);
+        }
+    }
+
+    private void DrawEdge(Vector3 _pos1, Vector3 _pos2) {
+        Debug.DrawLine(_pos1, _pos2, Color.green);
+    }
 #endregion
 }
 
-#if UNITY_EDITOR
 [CustomEditor(typeof(PathfindingVisualizer))]
 public class PathfindingVisualizerEditor : Editor {
-    SerializedProperty graphNodeHandle;
+    public SerializedProperty debug;
+    private Transform m_Obj;
 
     private void OnEnable() {
-        graphNodeHandle = serializedObject.FindProperty("GraphNodeHandle");
+        debug = serializedObject.FindProperty("debug");
+        m_Obj = ((PathfindingVisualizer)target).transform;
     }
 
     public override void OnInspectorGUI() {
         serializedObject.Update();
-        EditorGUILayout.PropertyField (graphNodeHandle, new GUIContent ("GraphNodeHandle"));
-        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.PropertyField(debug, new GUIContent("Draw Graph"));
+        if (GUILayout.Button("Export Graph")) {
+            Debug.Log("Building graph from children of "+m_Obj.gameObject.name);
+            Graph _graph = new Graph();
+            foreach (Transform _t in m_Obj) {
+                GraphNode _node = _t.GetComponent<GraphNode>();
+                if (_node == null) continue;
+                BuildGraph(ref _graph, _node);
+            }
+            string _json = _graph.ToJsonString();
+            Debug.Log("Graph finished building with result: "+_json);
+            SaveGraph(_graph);
+        }
+        serializedObject.ApplyModifiedProperties(); 
+    }
+
+    private void BuildGraph(ref Graph _graph, GraphNode _graphNode) {
+        Node _parent = new Node(new NetworkVector3(_graphNode.transform.position.x, _graphNode.transform.position.y, _graphNode.transform.position.z));
+        int _parentIndex = _graph.AddNode(_parent);
+        if (_parentIndex == -1) return;
+        foreach (GraphNode _n in _graphNode.neighbors) {
+            Node _node = new Node(new NetworkVector3(_n.transform.position.x, _n.transform.position.y, _n.transform.position.z));
+            int _childIndex = _graph.AddNode(_node);
+            if (_childIndex == -1) continue;
+            _graph.LinkNodes(_parentIndex, _childIndex);
+            BuildGraph(ref _graph, _n);
+        }
+    }
+
+    private void SaveGraph(Graph _graph) {
+        string fileName = Application.dataPath+"/Resources/AIWaypointGraphs/save.json";
+        Files.CreateDirectoryIfNull(Application.dataPath+"/Resources");
+        Files.CreateDirectoryIfNull(Application.dataPath+"/Resources/AIWaypointGraphs");
+        Files.CreateFileIfNull(fileName, string.Empty);
+        Files.WriteToFile(fileName, _graph.ToJsonString(), false);
     }
 }
-#endif
