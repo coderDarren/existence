121cca5a6bf7d7380208746a88221f624e45954a
Boxcar87 <Boxcar87@gmail.com>
2020-05-30 13:56:31 -0400

    Added Raycast logic to Test equip item. Currently Object rotation along the Z axis seems to not be factored. Scale is also off. Need to look into Actual location of vertices vs rendered location

--- Existence/Assets/ArmorTest.cs
+++ Existence/Assets/ArmorTest.cs
@@ -14,41 +14,68 @@ using UnityEngine;
     private GameObject targetParent;
     private GameObject targetPart;
     private Vector3[] vert;
-    private Vector3[] baseVert;
+    private Vector3[] equipVert;
+    private Mesh equipMesh;
     private Mesh mesh;
+    private RaycastHit hit;
+    public LayerMask layerName;    
 
     void Start(){
-        //mesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
-        //baseVert = mesh.vertices;
+        
+        
         targetPart = transform.Find(partString).gameObject;
         targetParent = transform.FindDeepChild(partString + "_Pros").gameObject;
-        mat = targetPart.GetComponent<Renderer>();        
+        mat = targetPart.GetComponent<Renderer>();
+        mesh = targetPart.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        //layerName = LayerMask.GetMask("Mesh");
+        //Debug.Log(LayerMask.LayerToName(layerName)); 
         
     }
     
     void Update(){
-        if(Input.GetKeyDown(KeyCode.R)){
+        if(Input.GetKey(KeyCode.R)){
             EquipArmor();
         }
         if(Input.GetKeyDown(KeyCode.T)){
             ResetMesh();
-        }       
+        }
+        
     }
 
     void EquipArmor(){
         
-        /*vert = mesh.vertices;               
-        for(int i = 0; i < vert.Length; i++){
-            vert[i] += mesh.normals[i] * 0.1f * 0.1f * 0.1f * 0.2f;
-             
-            
-            
-        }z
-        mesh.vertices = vert;*/
-        mat.enabled = false;
-        
         prostheticI = Instantiate(prosthetic, targetParent.transform);
-        prostheticI.transform.localPosition = new Vector3(0f,0f,0f);           
+        prostheticI.transform.localPosition = new Vector3(0f,0f,0f);
+        equipMesh = prostheticI.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+        equipVert = equipMesh.vertices;
+        
+
+        for(int i = 0; i < equipVert.Length; i++){
+            equipMesh = prostheticI.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+            equipVert = equipMesh.vertices;
+            //Debug.DrawLine(transform.TransformPoint(equipVert[i]), -equipMesh.normals[i] + transform.TransformPoint(equipVert[i]), Color.green, 20f);           
+            Debug.DrawRay(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]), Color.blue, 20f);
+            
+                
+            if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(-equipMesh.normals[i]),out hit, 5, layerName)){
+                equipMesh.vertices[i] -= equipMesh.normals[i];
+                
+                Debug.Log(hit.point + " : -hit");
+                Debug.Log(hit.collider.gameObject.name);
+            }
+             
+            if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]),out hit, 5, layerName)){
+                //equipMesh.vertices[i] = equipMesh.normals[i] * 0.1f;
+                
+                Debug.Log(hit.point + " : +hit");
+                
+            }
+            
+        }
+        //mesh.vertices = vert;
+        //mat.enabled = false;
+        
+                   
         //prostheticI.transform.localRotation = targetParent.transform.rotation;
 
     }
