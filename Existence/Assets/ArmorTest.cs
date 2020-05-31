47cb8646a03a6c1c23c3377050b80aca7b304fb8
Boxcar87 <Boxcar87@gmail.com>
2020-05-31 13:32:43 -0400

    Added Holo_VFX

--- Existence/Assets/ArmorTest.cs
+++ Existence/Assets/ArmorTest.cs
@@ -28,53 +28,60 @@ using UnityEngine;
         mat = targetPart.GetComponent<Renderer>();
         mesh = targetPart.GetComponent<SkinnedMeshRenderer>().sharedMesh;
         //layerName = LayerMask.GetMask("Mesh");
-        //Debug.Log(LayerMask.LayerToName(layerName)); 
+        //Debug.Log(LayerMask.LayerToName(layerName));
+        
         
     }
     
     void Update(){
+        
+
         if(Input.GetKey(KeyCode.R)){
             EquipArmor();
         }
         if(Input.GetKeyDown(KeyCode.T)){
             ResetMesh();
         }
-        
+        if(prostheticI){  
+            equipMesh = prostheticI.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+            equipVert = equipMesh.vertices;
+            for(int i = 0; i < equipVert.Length; i++){
+                
+                //Debug.DrawLine(transform.TransformPoint(equipVert[i]), -equipMesh.normals[i] + transform.TransformPoint(equipVert[i]), Color.green, 20f);           
+                Debug.DrawRay(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]), Color.blue, 20f);
+                
+                    
+                if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(-equipMesh.normals[i]),out hit, 5, layerName)){
+                    equipMesh.vertices[i] -= equipMesh.normals[i];
+                    
+                    Debug.Log(hit.point + " : -hit");
+                    Debug.Log(hit.collider.gameObject.name);
+                }
+                
+                if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]),out hit, 5, layerName)){
+                    //equipMesh.vertices[i] = equipMesh.normals[i] * 0.1f;
+                    
+                    Debug.Log(hit.point + " : +hit");
+                    
+                }
+                
+            }
+        }
     }
 
     void EquipArmor(){
-        
+                
         prostheticI = Instantiate(prosthetic, targetParent.transform);
         prostheticI.transform.localPosition = new Vector3(0f,0f,0f);
+        
+        
         //prostheticI.transform.rotation =
-        equipMesh = prostheticI.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
-        equipVert = equipMesh.vertices;
+        
         
 
         
 
-        for(int i = 0; i < equipVert.Length; i++){
-            equipMesh = prostheticI.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
-            equipVert = equipMesh.vertices;
-            //Debug.DrawLine(transform.TransformPoint(equipVert[i]), -equipMesh.normals[i] + transform.TransformPoint(equipVert[i]), Color.green, 20f);           
-            Debug.DrawRay(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]), Color.blue, 20f);
-            
-                
-            if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(-equipMesh.normals[i]),out hit, 5, layerName)){
-                equipMesh.vertices[i] -= equipMesh.normals[i];
-                
-                Debug.Log(hit.point + " : -hit");
-                Debug.Log(hit.collider.gameObject.name);
-            }
-             
-            if(Physics.Raycast(prostheticI.transform.TransformPoint(equipVert[i]), prostheticI.transform.TransformDirection(equipMesh.normals[i]),out hit, 5, layerName)){
-                //equipMesh.vertices[i] = equipMesh.normals[i] * 0.1f;
-                
-                Debug.Log(hit.point + " : +hit");
-                
-            }
-            
-        }
+        
         //mesh.vertices = vert;
         //mat.enabled = false;
         
