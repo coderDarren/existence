9df7a1ae3c62f36c878877ea8bd8e510d202eb6c
Boxcar87 <Boxcar87@gmail.com>
2020-08-14 00:07:18 -0400

    Created portal effect for holo room. Created Holoroom controller for selecting destinations or local combat.

---
+++ Existence/Assets/HoloController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloController : Selectable
+{
+    public GameObject[] portal;
+
+    public void Beach(){
+        for(int i=0;i<portal.Length;i++){
+            if(portal[i].name == "Beach")
+                portal[i].SetActive(true);
+            else{
+                portal[i].SetActive(false);
+            }
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    public void Combat(){
+        for(int i=0;i<portal.Length;i++){            
+                portal[i].SetActive(false);            
+        }
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Existence/Assets/Scripts/Game/PortalMat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMat : MonoBehaviour
+{   
+    public Camera portalCamera;
+    public Material portalMat; 
+
+    private void Start(){
+        
+        if(portalCamera.targetTexture != null)
+            portalCamera.targetTexture.Release();
+            
+        portalCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        portalMat.mainTexture = portalCamera.targetTexture;
+    }
+}
