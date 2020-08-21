ba73d33e0ada15e7b88ac025855a0abeec6b38ff
Boxcar87 <Boxcar87@gmail.com>
2020-08-21 15:08:16 -0400

    added anim controllers for network

--- Existence/Assets/HoloController.cs
+++ Existence/Assets/HoloController.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class HoloController : Selectable
 {
     public GameObject[] portal;
+    public Camera[] portalSceneCamera;
+    public Material[] sceneMat;
+    public GameObject holoWindow;
+    
+
+    private void Start(){
+        
+            
+        
+    }
 
     public void Beach(){
         for(int i=0;i<portal.Length;i++){
-            if(portal[i].name == "Beach")
+            if(portal[i].name == "Beach"){
                 portal[i].SetActive(true);
+                //holoWindow.GetComponent<Renderer>().material = sceneMat[i];
+                /*if(portalSceneCamera[i].targetTexture != null)
+                    portalSceneCamera[i].targetTexture.Release();
+                portalSceneCamera[i].targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+                sceneMat[i].mainTexture = portalSceneCamera[i].targetTexture;*/
+            }
             else{
                 portal[i].SetActive(false);
             }
-        }
-        this.gameObject.SetActive(false);
+        }        
+        this.gameObject.SetActive(false);        
     }
 
     public void Combat(){
         for(int i=0;i<portal.Length;i++){            
                 portal[i].SetActive(false);            
         }
+        //holoWindow.GetComponent<Renderer>().material = sceneMat[sceneMat.Length - 1];
         this.gameObject.SetActive(false);
     }
 }
