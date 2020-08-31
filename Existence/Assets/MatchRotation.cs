3fcc3b068c11e60514a5a9ae197ded188703198f
Boxcar87 <Boxcar87@gmail.com>
2020-08-31 15:15:58 -0400

    setup light settings for scene, set all light probes, added player specific lighting. PRE BAKE push

---
+++ Existence/Assets/MatchRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchRotation : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.rotation = GameObject.FindGameObjectWithTag("Player").transform.rotation;
+    }
+}
