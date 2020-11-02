56a8ab50927399be8c31b7d0467ed04db3cc8188
Boxcar87 <Boxcar87@gmail.com>
2020-11-02 06:07:43 -0500

    Tailored up materials to match current shaders used. Added more immersive lighting to holo room. Tailored mob prefabs a bit more. Added roar to Gasteroid

---
+++ Existence/Assets/Scripts/Tools/KeyStrokeTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyStrokeTest : MonoBehaviour
+{    
+    public KeyCode stroke;
+    public GameObject target;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(stroke))
+            RunEvent();
+    }
+
+    private void RunEvent(){
+        target.GetComponent<Animator>().SetTrigger("Test");
+    }
+}
---
+++ Existence/Assets/Scripts/Tools/MobLookAt.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobLookAt : MonoBehaviour
+{
+    public float range;
+    
+    private Transform m_Player;
+    private Animator m_Animator;
+    private float m_Distance;
+    private bool m_Aggro;
+    private bool m_Look;
+    
+    private void Start(){
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void Update(){
+        if (!m_Player)
+            m_Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        m_Distance = Vector3.Distance(transform.position, m_Player.position);
+        
+        MobLook();   
+        Debug.Log(m_Distance);
+        Debug.Log(m_Look);             
+    }
+
+    private void MobLook(){   
+        if (m_Distance < range)
+            m_Look = true;
+        
+        else if(m_Distance > 50){
+            m_Aggro = false;
+            m_Look = false;
+        }
+
+        if (m_Look){
+            if (!m_Aggro){                        
+                m_Aggro = true;
+                m_Animator.SetTrigger("Aggro");
+            }
+            transform.LookAt(m_Player);
+        }
+    }
+
+    public void PlaySoundEffect(){
+        GetComponent<AudioSource>().Play();
+    }
+}
